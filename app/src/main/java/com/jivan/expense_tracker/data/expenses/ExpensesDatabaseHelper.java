2c43848f00ea32bd2c5b9010d09a609557c877f0
Jivan Kadel <jivankadel256@gmail.com>
2025-05-13 10:12:41 +0545

    DB Helper for expense table

--- app/src/main/java/com/jivan/expense_tracker/data/expenses/ExpensesDatabaseHelper.java
+++ app/src/main/java/com/jivan/expense_tracker/data/expenses/ExpensesDatabaseHelper.java
@@ -1,4 +1,27 @@
 package com.jivan.expense_tracker.data.expenses;
 
-public class ExpensesDatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ExpensesDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "expenses.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public ExpensesDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(ExpensesContract.CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // For now, simply drop and recreate the table if version changes
+        db.execSQL("DROP TABLE IF EXISTS " + ExpensesContract.TABLE_NAME);
+        onCreate(db);
+    }
 }
