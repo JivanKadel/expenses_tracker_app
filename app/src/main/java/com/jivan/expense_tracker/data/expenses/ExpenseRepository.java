4202b7932c72f93229ca5dce1a9a4f394d1da373
Jivan Kadel <jivankadel256@gmail.com>
2025-05-15 09:34:25 +0545

    added method to get expense by id

--- app/src/main/java/com/jivan/expense_tracker/data/expenses/ExpenseRepository.java
+++ app/src/main/java/com/jivan/expense_tracker/data/expenses/ExpenseRepository.java
@@ -85,10 +85,60 @@ public class ExpenseRepository {
         return expenses;
     }
 
+    public Expense getExpenseById(int id) {
+        db = dbHelper.getReadableDatabase();
+        Expense expense = null;
+        String query = "SELECT e.*, c." + CategoryContract.COLUMN_NAME + ", c." + CategoryContract.COLUMN_GROUP +
+                ", c." + CategoryContract.COLUMN_IS_CUSTOM + " FROM " + ExpensesContract.TABLE_NAME +
+                " e " + " INNER JOIN " + CategoryContract.TABLE_NAME + " c " +
+                " On e." + ExpensesContract.COLUMN_CATEGORY_ID + " = c." + CategoryContract.COLUMN_ID +
+                " WHERE e." + ExpensesContract.COLUMN_ID + " = ?";
+
+        Cursor cursor = db.rawQuery(query, new String[]{String.valueOf(id)});
+
+        if (cursor.moveToFirst()) {
+            int cIdIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_CATEGORY_ID);
+            int cNameIndex = cursor.getColumnIndexOrThrow(CategoryContract.COLUMN_NAME);
+            int cGroupIndex = cursor.getColumnIndexOrThrow(CategoryContract.COLUMN_GROUP);
+            int cIsCustomIndex = cursor.getColumnIndexOrThrow(CategoryContract.COLUMN_IS_CUSTOM);
+
+            Category category = new Category(
+                    cursor.getInt(cIdIndex),
+                    cursor.getString(cNameIndex),
+                    cursor.getString(cGroupIndex),
+                    cursor.getInt(cIsCustomIndex) == 1);
+
+            int eIdIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_ID);
+            int eTitleIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_TITLE);
+            int eAmountIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_AMOUNT);
+            int eDateIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_DATE);
+            int ePaymentMethodIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_PAYMENT_METHOD);
+            int eNoteIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_NOTE);
+            int eCurrencyIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_CURRENCY);
+            int eIsRecurringIndex = cursor.getColumnIndexOrThrow(ExpensesContract.COLUMN_IS_RECURRING);
+
+            expense = new Expense(
+                    cursor.getInt(eIdIndex),
+                    cursor.getString(eTitleIndex),
+                    cursor.getDouble(eAmountIndex),
+                    category,
+                    Helper.stringToDate(cursor.getString(eDateIndex)),
+                    cursor.getString(ePaymentMethodIndex),
+                    cursor.getString(eNoteIndex),
+                    cursor.getString(eCurrencyIndex),
+                    cursor.getInt(eIsRecurringIndex) == 1
+            );
+        }
+        cursor.close();
+        return expense;
+    }
+
+
     public int deleteExpenseById(int expenseId) {
         db = dbHelper.getWritableDatabase();
         return db.delete(ExpensesContract.TABLE_NAME, ExpensesContract.COLUMN_ID + " = ?", new String[]{String.valueOf(expenseId)});
     }
+
     public int updateExpense(Expense expense) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
