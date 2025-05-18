62c685d91360e8237931c4ffc44554cec679e893
Jivan Kadel <jivankadel256@gmail.com>
2025-05-18 09:26:10 +0545

    update currency using user's preferences

--- app/src/main/java/com/jivan/expense_tracker/ui/expenses/ExpenseAdapter.java
+++ app/src/main/java/com/jivan/expense_tracker/ui/expenses/ExpenseAdapter.java
@@ -1,5 +1,8 @@
 package com.jivan.expense_tracker.ui.expenses;
 
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.jivan.expense_tracker.R;
 import com.jivan.expense_tracker.domain.expenses.Expense;
+import com.jivan.expense_tracker.util.format.CurrencyFormatter;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,6 +21,7 @@ import java.util.List;
 import java.util.Locale;
 
 public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseViewHolder> {
+
 
     public interface OnExpenseClickListener {
         void onExpenseClick(Expense expense);
@@ -45,10 +50,6 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseV
         holder.bind(expense, listener);
     }
 
-    //    @Override
-//    public int getItemCount() {a
-//        return expenses.size();
-//    }
     @Override
     public int getItemCount() {
         return expenses != null ? expenses.size() : 0;
@@ -57,17 +58,26 @@ public class ExpenseAdapter extends RecyclerView.Adapter<ExpenseAdapter.ExpenseV
 
     public static class ExpenseViewHolder extends RecyclerView.ViewHolder {
         private final TextView expenseTitle, expenseAmount, expenseDate;
+        private final CurrencyFormatter cf;
+        private SharedPreferences userPreferences;
 
         public ExpenseViewHolder(@NonNull View itemView) {
             super(itemView);
+            userPreferences = itemView.getContext().getSharedPreferences("user_settings", Context.MODE_PRIVATE);
+
+            cf = new CurrencyFormatter(itemView.getContext());
             expenseTitle = itemView.findViewById(R.id.expenseTitle);
             expenseAmount = itemView.findViewById(R.id.expenseAmount);
             expenseDate = itemView.findViewById(R.id.expenseDate);
         }
 
         public void bind(final Expense expense, final OnExpenseClickListener listener) {
+
+            float exchangeRate = Float.parseFloat(userPreferences.getString("euro_base_exchange_rate", "1.0"));
+            String currency = userPreferences.getString("currency", "eur");
+
             expenseTitle.setText(expense.getTitle());
-            expenseAmount.setText(String.format(Locale.getDefault(), "%.2f %s", expense.getAmount(), expense.getCurrency()));
+            expenseAmount.setText(String.format(Locale.getDefault(), "%.2f %s", expense.getAmount() * exchangeRate, currency));
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
             expenseDate.setText(sdf.format(expense.getDate()));
 
